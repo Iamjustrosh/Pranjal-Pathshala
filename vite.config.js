fffbfbae69cf917ec2685a77b5fcad9a2c247e1c
Iamjustrosh <roshanjain.220407@gmail.com>
2026-09-14 00:03:48 +0530

    fix: vitepwa workbox size

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa';
-import { fileURLToPath, URL } from 'node:url';
+import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
-  plugins: [react(), 
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  plugins: [
+    react(),
     tailwindcss(),
+
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+
       manifest: {
         name: 'Pranjal Pathshala',
         short_name: 'Pranjal Pathshala',
-        description: 'A simple platform for study materials and admissions.',
+        description:
+          'A simple platform for study materials and admissions.',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
         start_url: '/',
+
         icons: [
           {
             src: 'pwa-192x192.png',
@@ -34,7 +54,5 @@ export default defineConfig({
         ],
       },
     }),
-
-
   ],
 })
